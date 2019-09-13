a54684cafb9fa7e545ca046e4f7a76cb250b7bbf
New-Angelo <54410977+New-Angelo@users.noreply.github.com>
2019-09-12 23:31:05 -0300

    Update 1.c

--- Lista 2/1.c
+++ Lista 2/1.c
@@ -5,6 +5,8 @@
 struct complex{
 	float real;
 	float imag;
+	float z;
+	float theta;
 };
 typedef struct complex Complex;
 
@@ -63,26 +65,36 @@ Complex divi(Complex c1, Complex c2){
 	return c3;
 }
 
+//letra h
+float angulo(Complex c1){
+	float angulo;
+	angulo = atan(c1.imag/c1.real);
+	return angulo;
+}
+
 int main(){
 	
 	Complex c1 = {4,5};
 	Complex c2 = {6,5};
 	Complex c3 = soma(c1,c2);
 	
-	printf("c1 real: %.2f c1 imag:%.2f\n",c1.real, c1.imag);
-	printf("c2 real: %.2f c2 imag:%.2f\n",c2.real, c2.imag);
-	printf("c3 real: %.2f c3 imag:%.2f\n",c3.real, c3.imag);
+	printf("(c1) real: %.2f imag:%.2f\n",c1.real, c1.imag);
+	printf("(c2) real: %.2f imag:%.2f\n",c2.real, c2.imag);
+	printf("(c3) real: %.2f imag:%.2f\n",c3.real, c3.imag);
 	c3 = sub(c1,c2);
 	printf("\n**Subtracao**\n real: %.2f imag:%.2f\n",c3.real, c3.imag);
 	c3 = multesc(2,c1);
-	printf("\n**Multiplicacao por Escalar(2)**\n real: %.2f imag:%.2f\n", c3.real, c3.imag);
+	printf("\n**Multiplicacao por Escalar(2 X C1)**\n real: %.2f imag:%.2f\n", c3.real, c3.imag);
 	c3 = mult(c1,c2);
 	printf("\n**Multiplicacao c1 X c2**\n real: %.2f imag:%.2f\n", c3.real, c3.imag);
+
 	printf("\n**Norma de C1**\n %.2f\n", norma(c1));
 	c3 = conj(c2);
-	printf("\n**Conjugado de C2**\n real: %.2f imag:%.2f\n", c3.real, c3.imag);
+	printf("\n**Conjugado de C2**\nreal: %.2f imag:%.2f\n", c3.real, c3.imag);
 	c3 = divi(c1,c2);
-	printf("\n**divisao c1 X c2**\n real: %.2f imag:%.2f\n", c3.real, c3.imag);
+	printf("\n**Divisao c1 por c2**\nreal: %.2f imag:%.2f\n", c3.real, c3.imag);
+	c1.theta = angulo(c1);
+	c1.z = norma(c1);
+	printf("\n**Adicionado a norma e o angulo de c1**\nreal: %.2f imag:%.2f norma:%.2f angulo:%.2f\n",c1.real,c1.imag,c1.z,c1.theta);
   
 	return 0;	
-}
